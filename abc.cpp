0368b3719bbb71c02762601c779b2c51e032b4f6
Pham Ngoc Duy <duysm17102004@gmail.com>
2023-10-12 23:31:48 +0700

    test

--- abc.cpp
+++ abc.cpp
@@ -1,126 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-long long F[1000][1000]={0};
-int main(){
-    fstream input("input.txt", ios::in);
-    fstream out("output.txt", ios::out);
-    if (!input){
-        cout << "Khong doc duoc file !\n";
-        return 0;
+
+const int MAXN = 1e5 + 5;
+
+struct Query {
+    int left, right, index;
+};
+
+int a[MAXN];
+int result[MAXN];
+int freq[2 * MAXN];
+int smallestAbsent = 1;
+
+bool cmp(Query a, Query b) {
+    int blockSize = sqrt(MAXN);
+    if (a.left / blockSize != b.left / blockSize) {
+        return a.left / blockSize < b.left / blockSize;
     }
-    input>>n;
-    input.close();
-    int nA = 0;
-	int sum = n*(n+1)/2;
-    if(sum%2!=0)
-        out << 0;
-    else{
-        F[0][0] = 1;
-        for (int i=1; i<=n; i++){
-            for (int j=0; j<=sum/2; j++){
-                F[i][j] = F[i-1][j];
-                if(j>=i)
-                    F[i][j] += F[i-1][j-i];
-            }
-        }
-        out << F[n][sum/2]/2 << endl;
-    }
-    for (int i=0; i<=n; i++){
-        for (int j=0; j<=sum/2; j++)
-            out << F[i][j] << "\t";
-        out << "\n";
-    }
-    out.close();
+    return a.right < b.right;
 }
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// #include <cmath>
-// #include <unordered_set>
 
-// using namespace std;
+void add(int x) {
+    freq[x]++;
+    while (freq[smallestAbsent] > 0) {
+        smallestAbsent++;
+    }
+}
 
-// const int MAXN = 5e5 + 5;
-// const int MAXQ = 5e5 + 5;
-// const int MAX_VALUE = 1e9 + 5;
+void remove(int x) {
+    freq[x]--;
+    if (x < smallestAbsent) {
+        smallestAbsent = x;
+    }
+}
 
-// struct Query {
-//     int left, right, index;
-// };
+int main() {
+    int n, q;
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
 
-// int blockSize;
-// vector<int> a(MAXN);
-// vector<int> result(MAXQ);
-// unordered_set<int> distinctNumbers;
+    vector<Query> queries(q);
+    for (int i = 0; i < q; i++) {
+        cin >> queries[i].left >> queries[i].right;
+        queries[i].index = i;
+    }
 
-// void add(int number) {
-//     distinctNumbers.insert(number);
-// }
+    sort(queries.begin(), queries.end(), cmp);
 
-// void remove(int number) {
-//     distinctNumbers.erase(number);
-// }
+    int currentLeft = 1, currentRight = 0;
 
-// bool cmp(const Query &a, const Query &b) {
-//     int blockA = a.left / blockSize;
-//     int blockB = b.left / blockSize;
-//     if (blockA != blockB) {
-//         return blockA < blockB;
-//     }
-//     return a.right < b.right;
-// }
+    for (Query query : queries) {
+        int left = query.left;
+        int right = query.right;
 
-// int main() {
-//     int n, Q;
-//     cin >> n >> Q;
+        while (currentLeft < left) {
+            remove(a[currentLeft]);
+            currentLeft++;
+        }
 
-//     blockSize = static_cast<int>(sqrt(n));
-//     vector<Query> queries;
+        while (currentLeft > left) {
+            add(a[currentLeft - 1]);
+            currentLeft--;
+        }
 
-//     for (int i = 0; i < n; i++) {
-//         cin >> a[i];
-//     }
+        while (currentRight < right) {
+            add(a[currentRight + 1]);
+            currentRight++;
+        }
 
-//     for (int i = 0; i < Q; i++) {
-//         int L, R;
-//         cin >> L >> R;
-//         queries.push_back({L - 1, R - 1, i});
-//     }
+        while (currentRight > right) {
+            remove(a[currentRight]);
+            currentRight--;
+        }
 
-//     sort(queries.begin(), queries.end(), cmp);
+        result[query.index] = smallestAbsent;
+    }
 
-//     int left = 0, right = -1;
+    for (int i = 0; i < q; i++) {
+        cout << result[i] << endl;
+    }
 
-//     for (const Query &query : queries) {
-//         while (left > query.left) {
-//             left--;
-//             add(a[left]);
-//         }
-//         while (right < query.right) {
-//             right++;
-//             add(a[right]);
-//         }
-//         while (left < query.left) {
-//             remove(a[left]);
-//             left++;
-//         }
-//         while (right > query.right) {
-//             remove(a[right]);
-//             right--;
-//         }
-
-//         int smallestMissing = 0;
-//         while (distinctNumbers.count(smallestMissing) > 0) {
-//             smallestMissing++;
-//         }
-
-//         result[query.index] = smallestMissing;
-//     }
-
-//     for (int i = 0; i < Q; i++) {
-//         cout << result[i] << endl;
-//     }
-
-//     return 0;
-// }
+    return 0;
+}
--- dasa.cpp
+++ dasa.cpp
@@ -10,35 +10,151 @@ struct NODE{
 struct TREE{
     NODE* root;
 };
+
+void postOrder(NODE* root);
+void inOrder(NODE* root);
+void preOrder(NODE* root);
+void Add(NODE* &root, int x);
+NODE* SearchOnTree(NODE* root, int x);
+int DeleteNode(NODE* root, int x);
+void deleteAll(NODE* &root);
+NODE* createNode(int x);
+
 int a[100], n;
 int main(){
-    fstream in("input.txt", ios::in);
-    if(!in){
-		cout<<"Khong the doc file";
-		return 0;
-	}
-    
-    in >> n;
+    cin >> n;
     for (int i=0; i<n; i++)
-        in >> a[i];
-    in.close();
+        cin >> a[i];
     TREE tr;
     tr.root = NULL;
-    tr.root->left = NULL;
-    tr.root->right = NULL;
-    tr.root->data = a[0];
-    for (int i=1; i<n; i++)
+    for (int i=0; i<n; i++){
         Add(tr.root, a[i]);
+    }
+    preOrder(tr.root);
+    cout << endl;
+    NODE* p = SearchOnTree(tr.root, 3);
+    cout << p->data << endl;
+    // inOrder(tr.root);
+    // cout << endl;
+    // postOrder(tr.root);
+    // cout << endl;
+    // DeleteNode(tr.root, 4);
+    // preOrder(tr.root);
+    // deleteAll(tr.root);
     return 0;
 }
-int Add(NODE* root, int x){
-    if (root!=NULL){
-        if (root->data==x)
-            return 0;
-        else if (root->data > x)
-            return Add(root->left, x);
-        else if (root->data < x)
-            return Add(root->right, x);   
+
+void xoaLa(NODE* root, int x){
+    if (root->left==NULL)
+        return;
+    else if (root->data > x)
+        xoaLa(root->left, x);
+    else if  (root->data < x)
+        xoaLa(root->right, x);
+    else{
+        NODE* p = root;
+        root = NULL;
+        delete p;
     }
 }
 
+int DeleteNode(NODE* root, int x){
+    if (root==NULL)
+        return 0;
+    if (root->data > x)
+        DeleteNode(root->left, x);
+    if (root->data < x)
+        DeleteNode(root->right, x);
+    NODE* tmp;
+    if (root->left==NULL){
+        tmp = root;
+        root = root->right;
+        delete tmp;
+    }
+    else{
+        if (root->right==NULL){
+            tmp = root;
+            root = root->left;
+            delete tmp;
+        }
+        else{   // trường hợp có đầy đủ 2 con
+            NODE* p;
+            for (p=root->right; p->left!=NULL; p=p->left);
+            tmp = p;
+            root->data =p->data;
+            p=p->right;
+            delete tmp;
+        }
+    }
+    return 1;
+}
+
+NODE* SearchOnTree(NODE* root, int x){
+    NODE* p = root;
+    while (p!=NULL){
+        if (p->data==x)
+            return p;
+        else if (p->data > x)
+            p=p->left;
+        else if (p->data < x)
+            p=p->right;
+    }
+    return NULL;
+}
+
+NODE* createNode(int x){
+    NODE* root = new NODE;
+    if (root==NULL)
+        return NULL;
+    else{
+        root->data = x;
+        root->left = NULL;
+        root->right = NULL;
+        return root;
+    }
+}
+void Add(NODE* &root, int x){
+    if (root!=NULL){
+        if (root->data==x)
+            return;
+        if (root->data > x)
+            return Add(root->left, x);
+        if (root->data < x)
+            return Add(root->right, x);   
+    }
+    else{
+        root = createNode(x);
+    }
+}
+
+void preOrder(NODE* root){
+    if (root!=NULL){
+        cout << root->data << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void inOrder(NODE* root){
+    if (root!=NULL){
+        preOrder(root->left);
+        cout << root->data << " ";
+        preOrder(root->right);
+    }
+}
+
+void postOrder(NODE* root){
+    if (root!=NULL){
+        preOrder(root->left);
+        preOrder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+void deleteAll(NODE* &root){
+    if (root != NULL){
+        deleteAll(root->left);
+        deleteAll(root->right);
+        delete root;
+    }
+}
